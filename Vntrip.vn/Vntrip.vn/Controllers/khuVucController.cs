267ee89ed1d81bf94cf403ac53204b016d570311
doquocphong <doquocphong321@gmail.com>
2020-12-17 11:48:05 +0700

    first commit

---
+++ Vntrip.vn/Vntrip.vn/Controllers/khuVucController.cs
@@ -0,0 +1,21 @@
+﻿using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vntrip.vn.Models;
+
+namespace Vntrip.vn.Controllers
+{
+    public class khuVucController : Controller
+    {
+        vntripDataContext db = new vntripDataContext();
+        // GET: khuVuc
+        public ActionResult Index(int? page)
+        {
+            var khuVuc = from kv in db.KhuVucs select kv;
+            return View(khuVuc.ToPagedList(page ?? 1,10));
+        }
+    }
+}
